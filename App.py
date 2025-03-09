111651033392180d2d3cf87607ee24127832ce42
av3wagner <67894780+av3wagner@users.noreply.github.com>
2025-03-09 08:34:41 +0100

    Update App.py

--- App.py
+++ App.py
@@ -155,11 +155,7 @@ __*Материалы этого проекта могут служить хор
 __Берлин, 16\.02\.2025__
 '''
 
-#with open("WagnerFoto.jpg ", "rb") as img_file:    
-#        img = "data:image/png;base64," + base64.b64encode(img_file.read()).decode()
-
-#with open("ASFEN_LOGO3.jpg", "rb") as img_file:
-#        img2 = "data:image/png;base64," + base64.b64encode(img_file.read()).decode()        
+# enter 1  
 
 def MdFileToStreamlit(MdFile):
     with open(MdFile, 'r', encoding='utf-8') as f:
